47ccfaaa507d23b3a54d14b2c0dd996825386e35
Brooks Patton <BrooksPatton@users.noreply.github.com>
2022-01-16 16:41:52 +0000

    testing that we can create a user

--- integration-tests/backend-tests/api.test.js
+++ integration-tests/backend-tests/api.test.js
@@ -1,8 +1,14 @@
 require("dotenv").config();
 const { default: axios } = require("axios");
+const jwt = require('jsonwebtoken');
+const knex = require('knex');
+const dbConfiguration = require("./knexfile");
+const bcrypt = require("bcrypt");
 
 const apiPort = process.env.API_PORT;
 const baseUrl = `${process.env.API_URI}:${apiPort}/api/v1`;
+const JWT_SECRET = process.env.JWT_SECRET;
+const db = knex(dbConfiguration);
 
 describe("todo api", () => {
   describe("User CRUD", () => {
@@ -17,10 +23,19 @@ describe("todo api", () => {
       expect(typeof data.id).toBe("number");
       expect(data.username).toBe(userToCreate.username);
       expect(typeof data.token).toBe("string");
-      // check the token to make sure that it is signed correctly
-      // check the database to make sure that our new user is there and the password is hashed correcly
+      const token = jwt.verify(data.token, JWT_SECRET);
+      expect(token.username).toBe(userToCreate.username);
+      expect(typeof token.id).toBe('number');
+      expect(token.id).toBe(data.id);
+      const createdUser = await db.table("users").where("username", userToCreate.username).first();
+      expect(createdUser.id).toBe(data.id);
+      expect(createdUser.username).toBe(userToCreate.username);
+      expect(createdUser.password).not.toBe(userToCreate.password);
+      const passwordMatched = bcrypt.compareSync(userToCreate.password, createdUser.password);
+      expect(passwordMatched).toBe(true);
     });
-
+  
+    test.todo("cannot create multiple users with the same user name");
     test.todo("sign in");
     test.todo("log out");
   })
