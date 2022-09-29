c5b74d952f17a319bbb671491ed8962d76b3d5ef
Brookzerker <brooks@brookzerker.com>
2022-09-29 07:55:31 -0600

    Resetting lesson

---
+++ frontend/rust/yew/lessons/setting_up_yewdux/README.md
@@ -0,0 +1,6 @@
+# Setting up Yewdux
+
+- [ ] Install Yewdux
+- [ ] Create counter store with default value
+- [ ] display counter
+- [ ] Create button to increment count
--- frontend/rust/yew/lessons/setting_up_yewdux/src/lib.rs
+++ frontend/rust/yew/lessons/setting_up_yewdux/src/lib.rs
@@ -1,39 +1,20 @@
-mod counter;
-mod display;
-mod router;
-mod store;
-
-use counter::Counter;
-use display::DisplayCount;
-use router::{switch, Route};
-use store::{init, YewduxStore};
 use yew::prelude::*;
-use yew_router::prelude::*;
-use yewdux::prelude::*;
 
-pub struct App {
-    _dispatch: Dispatch<BasicStore<YewduxStore>>,
-}
+pub struct App {}
 
 impl Component for App {
     type Message = ();
 
-    type Properties = DispatchProps<BasicStore<YewduxStore>>;
+    type Properties = ();
 
     fn create(_ctx: &Context<Self>) -> Self {
-        let _dispatch = init();
-        Self { _dispatch }
+        Self {}
     }
 
     fn view(&self, _ctx: &Context<Self>) -> Html {
         html! {
             <div>
                 <h1>{"App"}</h1>
-                <WithDispatch<Counter> />
-                <WithDispatch<DisplayCount> />
-                <BrowserRouter>
-                    <Switch<Route> render={Switch::render(switch)} />
-                </BrowserRouter>
             </div>
         }
     }
