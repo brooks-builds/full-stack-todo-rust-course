c0fe050494a740223278a1d6b48d4d8488fd4054
Brookzerker <brooks@brookzerker.com>
2022-10-10 08:19:17 -0600

    Created middleware to get user.
    User is not yet being passed to next routes.

---
+++ backend/rust/learning_axum/src/routes/middleware.rs
@@ -0,0 +1,43 @@
+use crate::db;
+use crate::db::users::{self, Entity as Users};
+use crate::utilities::errors::AppError;
+use axum::{
+    http::{Request, StatusCode},
+    middleware::Next,
+    response::Response,
+};
+use sea_orm::{ColumnTrait, DatabaseConnection, EntityTrait, QueryFilter};
+
+pub async fn auth_required<T>(request: Request<T>, next: Next<T>) -> Result<Response, AppError> {
+    let key = request.headers().get("x-auth-token").ok_or_else(|| {
+        AppError::new(StatusCode::UNAUTHORIZED, eyre::eyre!("not authenticated!"))
+    })?;
+    let db = request
+        .extensions()
+        .get::<DatabaseConnection>()
+        .ok_or_else(|| {
+            AppError::new(
+                StatusCode::INTERNAL_SERVER_ERROR,
+                eyre::eyre!("Error getting database connection"),
+            )
+        })?;
+    match Users::find()
+        .filter(users::Column::Token.eq(Some(key.to_str().unwrap())))
+        .one(db)
+        .await
+    {
+        Ok(Some(user)) => Ok(next.run(request).await),
+        Ok(None) => {
+            return Err(AppError::new(
+                StatusCode::UNAUTHORIZED,
+                eyre::eyre!("Could not find user"),
+            ));
+        }
+        Err(error) => {
+            return Err(AppError::new(
+                StatusCode::INTERNAL_SERVER_ERROR,
+                eyre::eyre!(error),
+            ));
+        }
+    }
+}
--- backend/rust/learning_axum/src/routes/mod.rs
+++ backend/rust/learning_axum/src/routes/mod.rs
@@ -1,32 +1,31 @@
-use std::sync::Arc;
-
-use axum::{
-    routing::{get, post},
-    Extension, Router,
-};
-
 pub mod hello_world;
+mod middleware;
 mod tasks;
 mod users;
-
-use sea_orm::DatabaseConnection;
-use users::create_user;
-
-use crate::config::Config;
 
 use self::{
     tasks::create_task,
     users::{logout, sign_in},
 };
+use crate::config::Config;
+use axum::{
+    routing::{get, post},
+    Extension, Router,
+};
+use middleware::auth_required;
+use sea_orm::DatabaseConnection;
+use std::sync::Arc;
+use users::create_user;
 
 pub fn create_router(config: Arc<Config>, db: DatabaseConnection) -> Router {
     Router::new()
-        .route("/hello_world", get(hello_world::hello_world))
-        .route("/api/v1/users", post(create_user))
-        .route("/api/v1/users/login", post(sign_in))
         .route("/api/v1/users/logout", post(logout))
         .route("/api/v1/tasks", get(tasks::get_all_tasks))
         .route("/api/v1/tasks", post(create_task))
+        .layer(axum::middleware::from_fn(auth_required))
+        .route("/hello_world", get(hello_world::hello_world))
+        .route("/api/v1/users", post(create_user))
+        .route("/api/v1/users/login", post(sign_in))
         .layer(Extension(config))
         .layer(Extension(db))
 }
