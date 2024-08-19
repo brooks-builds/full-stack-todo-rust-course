81ab1b4588a0e0163e36eb2e27e2660524a2e724
Brookzerker <brooks@brookzerker.com>
2024-08-19 10:35:18 -0600

    updated axum route lessons to version 0.7.5

--- backend/rust/axum/lessons/routing/src/lib.rs
+++ backend/rust/axum/lessons/routing/src/lib.rs
@@ -1,12 +1,13 @@
 mod routes;
 
 use routes::create_routes;
+use tokio::net::TcpListener;
 
 pub async fn run() {
     let app = create_routes();
+    let listener = TcpListener::bind(&"0.0.0.0:3000").await.unwrap();
 
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
-        .serve(app.into_make_service())
+    axum::serve(listener, app.into_make_service())
         .await
         .unwrap();
 }
