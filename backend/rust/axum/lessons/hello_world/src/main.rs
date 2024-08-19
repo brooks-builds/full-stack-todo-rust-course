edc5cacdb32c44cf4f88c960bafa1e8d4b3b4558
Brookzerker <brooks@brookzerker.com>
2024-08-19 09:55:37 -0600

    updated hello world lesson to axum 0.7.5

--- backend/rust/axum/lessons/hello_world/src/main.rs
+++ backend/rust/axum/lessons/hello_world/src/main.rs
@@ -1,15 +1,16 @@
 use axum::{routing::get, Router};
+use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
     let app = Router::new().route("/", get(hello_world));
+    let listener = TcpListener::bind(&"0.0.0.0:3000").await.unwrap();
 
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
-        .serve(app.into_make_service())
+    axum::serve(listener, app.into_make_service())
         .await
         .unwrap()
 }
 
 async fn hello_world() -> String {
-    "Hello world!!!!!".to_owned()
+    "Hello world!".to_owned()
 }
